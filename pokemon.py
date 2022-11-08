960963e15512c15db54937ce40e7b5c2b9e71eec
Andrew <70990567+apakula036@users.noreply.github.com>
2022-11-08 11:13:25 -0800

    Update
    Can now grab user input and ask for a Pokémon and give its height and weight.

--- pokemon.py
+++ pokemon.py
@@ -3,10 +3,15 @@ import json
 from pokebase import cache, pokemon
 cache.API_CACHE
 
-pokemonData = requests.get('https://pokeapi.co/api/v2/pokemon/gloom')
+pokemonName = input("Enter Pokemon name:")
+print("You entered: " + pokemonName)
+
+
+pokemonData = requests.get('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
 # print(pokemonData.json)
-data = pokemonData.json()
+pokemon = pokemonData.json()
 # print(pokemonData.status_code)
-print(data())
+print(pokemon['name'])
+print(str(pokemon['height']) + " " + str(pokemon['weight']))
 
 
