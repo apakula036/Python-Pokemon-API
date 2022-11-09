77b6768e154c60c08de27b23a5e9c8bcf4fcc486
Andrew <70990567+apakula036@users.noreply.github.com>
2022-11-08 16:35:37 -0800

    Adding UI

--- pokemon.py
+++ pokemon.py
@@ -1,17 +1,34 @@
 import requests
 import json
+
+import tkinter as tk
+from tkinter import filedialog, Text
+import os 
+
 from pokebase import cache, pokemon
 cache.API_CACHE
 
-pokemonName = input("Enter Pokemon name:")
-print("You entered: " + pokemonName)
 
 
-pokemonData = requests.get('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
-# print(pokemonData.json)
-pokemon = pokemonData.json()
-# print(pokemonData.status_code)
-print(pokemon['name'])
-print(str(pokemon['height']) + " " + str(pokemon['weight']))
+def pokemonWeightandHeight():
+
+    pokemonName = input("Enter Pokemon name:")
+    print("You entered: " + pokemonName)
 
 
+    pokemonData = requests.get('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+    # print(pokemonData.json)
+    pokemon = pokemonData.json()
+    # print(pokemonData.status_code)
+    print(pokemon['name'])
+    print(str(pokemon['height']) + " " + str(pokemon['weight']))
+
+
+root = tk.Tk()
+canvas = tk.Canvas(root, height=700, width=700, bg="black")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+root.mainloop()
